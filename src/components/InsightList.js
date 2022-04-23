d09f53420d3847f67891fe239027e1c052a2c06d
Muskan Vaswan <muskanvaswan@gmail.com>
2022-04-23 16:29:13 +0530

    update color theme

--- src/components/InsightList.js
+++ src/components/InsightList.js
@@ -8,8 +8,9 @@ const HashButton = ({ address }) => {
   const humanize = (address) => (address.slice(0, 6) + "..." + address.slice(address.length - 6, address.length));
   return (
     <Button
-      sx={{textTransform: 'none', py: 0, px: 1, borderRadius: 5, bgcolor: 'black', color: 'white'}}
+      sx={{textTransform: 'none', py: 0, px: 1, borderRadius: 5, bgcolor: 'white', color: 'black', '&:hover': {bgcolor: 'grey', color: 'white'}}}
       onClick={() => {navigator.clipboard.writeText(address);}}
+
     >
       {humanize(address)}
     </Button>
@@ -18,7 +19,7 @@ const HashButton = ({ address }) => {
 const InsightCard = ({ insight, key }) => {
 
   return (
-    <Box key={key} sx={{borderRadius: 5, bgcolor: 'rgba(61, 61, 61, 0.14)', p: 2, my: 2, width: '100%', wordWrap: 'break-word'}}>
+    <Box key={key} sx={{borderRadius: 5, bgcolor: 'rgba(255, 255, 255, 0.14)', p: 2, my: 2, width: '100%', wordWrap: 'break-word'}}>
       <HashButton address={insight.sender} />
       <Typography sx={{fontSize: 15, lineHeight: '1.2em', mt: 1}}>{insight.insight}</Typography>
     </Box>
@@ -55,15 +56,16 @@ export default function InsightList({contractABI, contractAddress}) {
     const id = setInterval(getInsights, 3000)
     return () => clearInterval(id);
   }, [])
-  
+
   useEffect(() => {getInsights()}, []);
 
   return (
       <Box sx={{position: 'absolute', bottom: 0, right: 0, height: viewInsights? '40%': '8%', transition: 'height 1s ease-in-out',width: {md: '30%', xs: '100%'}, px: 2, pt:2, display: 'flex', overflowY: 'hidden'}}>
-        <Paper sx={{borderRadius: '10px 10px 0px 0px', bgcolor: 'white', color: 'black', height: '100%', width: '100%', px: 2, pb: 2,}} elevation={6}>
+        <Paper sx={{borderRadius: '10px 10px 0px 0px', bgcolor: 'black', color: 'white', height: '100%', width: '100%', px: 2, pb: 2,}} elevation={6}>
           <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
             <Typography variant="body1" sx={{fontWeight: 'bold'}}>Total Insights Given: {insights.length}</Typography>
             <IconButton
+              color="inherit"
               onClick={() => {setVeiwInsights(x => !x)}}>
               <KeyboardArrowDownIcon />
             </IconButton>
